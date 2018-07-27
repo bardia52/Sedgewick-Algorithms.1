5b49d66f1dfdc4ea0b6ccb14eafd64e967fb0cb9
Bardia Alavi <bardia_alavi@yahoo.com>
2018-07-26 22:12:35 -0400

    address all failures now 100/100 with 1 warning

--- Assignment.3-collinear/BruteCollinearPoints.java
+++ Assignment.3-collinear/BruteCollinearPoints.java
@@ -20,14 +20,18 @@ public class BruteCollinearPoints {
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points)
     {
-        checkDuplicatedEntries(points);
+        // Check corner cases
         checkNullEntries(points);
+        checkDuplicatedEntries(points);
         ArrayList<LineSegment> foundSegments = new ArrayList<>();
 
+        // We need a copy because we are sorting
         Point[] pointsCopy = Arrays.copyOf(points, points.length);
+
+        // Sorting points based on compareTo (first Y, then X)
         Arrays.sort(pointsCopy);
 
-        //double EPSILON = 1e-8;
+        // Loop over 4 points
         for (int p = 0; p < pointsCopy.length - 3; p++) {
             for (int q = p + 1; q < pointsCopy.length - 2; q++) {
                 for (int r = q + 1; r < pointsCopy.length - 1; r++) {
@@ -35,13 +39,7 @@ public class BruteCollinearPoints {
                         double pqSlope = pointsCopy[p].slopeTo(pointsCopy[q]);
                         double prSlope = pointsCopy[p].slopeTo(pointsCopy[r]);
                         double psSlope = pointsCopy[p].slopeTo(pointsCopy[s]);
-
-                        if ((pqSlope == prSlope) && (pqSlope == psSlope))
-                        /*if (Math.abs(pointsCopy[p].slopeTo(pointsCopy[q]) - 
-                            pointsCopy[p].slopeTo(pointsCopy[r])) == 0 &&
-                                pointsCopy[p].slopeTo(pointsCopy[q]) == 
-                                pointsCopy[p].slopeTo(pointsCopy[s]))*/
-                        {
+                        if ((pqSlope == prSlope) && (pqSlope == psSlope)) {
                             foundSegments.add(new LineSegment(pointsCopy[p], pointsCopy[s]));
                         }
                     }
@@ -68,7 +66,7 @@ public class BruteCollinearPoints {
         if (points == null) {
             throw new IllegalArgumentException("Null Entry");
         }
-        for (int i = 0; i < points.length - 1; i++) {
+        for (int i = 0; i < points.length; i++) {
             if (points[i] == null) {
                 throw new IllegalArgumentException("Null Entry");
             }
@@ -85,4 +83,5 @@ public class BruteCollinearPoints {
             }
         }
     }
+
 }
--- Assignment.3-collinear/FastCollinearPoints.java
+++ Assignment.3-collinear/FastCollinearPoints.java
@@ -16,24 +16,17 @@ import java.util.Arrays;
 public class FastCollinearPoints {
     private final ArrayList<LineSegment> jSegments = new ArrayList<>();
 
-    // finds all line segments containing 4 or more points
+    // Find all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points)
     {
-        // check corner cases
-        if (points == null)
-            throw new NullPointerException();
-        
+        // Check corner cases
+        checkNullEntries(points);
+        checkDuplicatedEntries(points);
+
         Point[] jCopy = points.clone();
         Arrays.sort(jCopy);
-        
-        if (hasDuplicate(jCopy))
-        {
-            throw new IllegalArgumentException("U have duplicate points");
-        }
-        // and now show must go on )))
 
-        for (int i = 0; i < jCopy.length - 3; i++)
-        {
+        for (int i = 0; i < jCopy.length - 3; i++) {
             Arrays.sort(jCopy);
 
             // Sort the points according to the slopes they makes with p.
@@ -86,17 +79,27 @@ public class FastCollinearPoints {
         return jSegments.toArray(new LineSegment[jSegments.size()]);
     }
 
-    // test the whole array fo duplicate points
-    private boolean hasDuplicate(Point[] points)
-    {
-        for (int i = 0; i < points.length - 1; i++)
-        {
-            if (points[i].compareTo(points[i + 1]) == 0)
-            {
-                return true;
+    private void checkNullEntries(Point[] points) {
+        if (points == null) {
+            throw new IllegalArgumentException("Null Entry");
+        }
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) {
+                throw new IllegalArgumentException("Null Entry");
             }
         }
-        return false;
+    }
+
+    private void checkDuplicatedEntries(Point[] points) {
+        for (int i = 0; i < points.length - 1; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].compareTo(points[j]) == 0) {
+                    throw new IllegalArgumentException(
+                    "Duplicated entries in given points.");
+                }
+            }
+        }
     }
 
 }
+
