f1d9786a67b98b1605c3ab69c270df2a61c6b26a
Bardia Alavi <bardia_alavi@yahoo.com>
2018-08-10 00:55:14 -0400

    Initial file creation

---
+++ Assignment.4-8puzzle/Board.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+ *  Name:    Bardia Alavi
+ *  NetID:   bardia
+ *  Precept: P01
+ *
+ *  Partner Name:    N/A
+ *  Partner NetID:   N/A
+ *  Partner Precept: N/A
+ * 
+ *  Description: A data-type to represent board having the 8-puzzle (n^2-1) puzzle.
+ ******************************************************************************/
+
