e682d2f7d3c375c3d7352c0920e50eadae46be40
Bardia Alavi <bardia_alavi@yahoo.com>
2018-08-11 01:03:59 -0400

    Create project and adding contructor body and hamming

--- Assignment.4-8puzzle/Board.java
+++ Assignment.4-8puzzle/Board.java
@@ -11,16 +11,79 @@
  ******************************************************************************/
 
 public class Board {
-    public Board(int[][] blocks)       // construct a board from an n-by-n array of blocks
-                                       // (where blocks[i][j] = block in row i, column j)
-    public int dimension()             // board dimension n
-    public int hamming()               // number of blocks out of place
-    public int manhattan()             // sum of Manhattan distances between blocks and goal
-    public boolean isGoal()            // is this board the goal board?
-    public Board twin()                // a board that is obtained by exchanging any pair of blocks
-    public boolean equals(Object y)    // does this board equal y?
-    public Iterable<Board> neighbors() // all neighboring boards
-    public String toString()           // string representation of this board (in the output format specified below)
+    private final int[][] internalBlocks;
+    private final int boardDim;
+    private int hammingScore = 0;
 
-    public static void main(String[] args) // unit tests (not graded)
+    // construct a board from an n-by-n array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks) {
+        int m = blocks.length;
+        int n = blocks[0].length;
+        if (m != n) {
+            throw new IllegalArgumentException("Non Equal Entry");
+        }
+        internalBlocks = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                this.internalBlocks[i][j] = blocks[i][j];
+            }
+        }
+        this.boardDim = n;
+    }
+
+    // board dimension n
+    public int dimension() {
+        return this.boardDim;
+    }
+
+    // number of blocks out of place
+    public int hamming() {
+        int newScore = 0;
+        int value = 0;
+        for (int i = 0; i < boardDim; i++) {
+            for (int j = 0; j < boardDim; j++) {
+                value = internalBlocks[i][j];
+                newScore += (value == (i * boardDim) + j) ? 0 : 1;
+            }
+        }
+        if (value == 0) // subtract last value
+            newScore--;
+        return (newScore + hammingScore);
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan() {
+        return 0;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+        return true;
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    /*public Board twin() {
+        return new Board(int [this.boardDim][this.boardDim]);
+    }*/
+
+    // does this board equal y?
+    public boolean equals(Object y) {
+        return true;
+    }
+
+    // all neighboring boards
+/*    public Iterable<Board> neighbors() {
+        
+    }
+*/
+    // string representation of this board (in the output format specified below)
+    public String toString() {
+        return "1";
+    }
+
+    // unit tests (not graded)
+    public static void main(String[] args) {
+        return;
+    }
 }
