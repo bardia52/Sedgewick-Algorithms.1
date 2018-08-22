50535e0cafe5b116eccef48c5ead0bb67bdcf7fd
bardia52 <bardia_alavi@yahoo.com>
2018-08-22 15:21:31 -0400

    Add twins added point class

--- Assignment.4-8puzzle/Board.java
+++ Assignment.4-8puzzle/Board.java
@@ -101,6 +101,9 @@ public class Board {
     // a board that is obtained by exchanging any pair of blocks
     public Board twin() {
         Board twinBoard = new Board(this);
+        point firstPoint = findANoneZeroPoint();
+        point secondPoint = findNextNoneZeroPoint(firstPoint);
+        swapPoints(twinBoard, firstPoint, secondPoint);
         return twinBoard;
     }
 
@@ -179,13 +182,26 @@ public class Board {
             Board initial = new Board(tiles);
             int hammingScore = initial.hamming();
             int manhatanScore = initial.manhattan();
-            String strB = initial.toString();
+            Board twinBoard = initial.twin();
+            String strA = initial.toString();
+            String strB = twinBoard.toString();
             // Solver solver = new Solver(initial);
             StdOut.println(filename + ": hamming = " + hammingScore + " : mahnatan = " + manhatanScore);
+            StdOut.print(strA);
             StdOut.print(strB);
             // StdOut.println(filename + ": " + solver.moves());
         }
         return;
+    }
+
+    private void swapPoints(Board board, point point1, point point2) {
+        int i1 = point1.geti();
+        int j1 = point1.getj();
+        int i2 = point2.geti();
+        int j2 = point2.getj();
+        int temp = board.internalBlocks[i1][j1];
+        board.internalBlocks[i1][j1] = board.internalBlocks[i2][j2];
+        board.internalBlocks[i2][j2] = temp;
     }
 
     private int findiZero() {
@@ -210,12 +226,45 @@ public class Board {
         return -1;
     }
 
+    private point findANoneZeroPoint() {
+        point noneZeroPoint = new point(0, 0, this.boardDim);
+        for (int i = 0; i < this.boardDim; i++) {
+            for (int j = 0; j < this.boardDim; j++) {
+                if (internalBlocks[i][j] != 0) {
+                    noneZeroPoint.setPoint(i, j);
+                    break;
+                }
+            }
+        }
+        return noneZeroPoint;
+    }
+
+    private point findNextNoneZeroPoint(point p) {
+        point nextPoint = new point(p);
+        int i = p.geti();
+        int j = p.getj();
+        if (checkLegit(i + 1, j))
+            nextPoint.setPoint(i + 1, j);
+        else if (checkLegit(i - 1, j))
+            nextPoint.setPoint(i - 1, j);
+        else if (checkLegit(i, j + 1))
+            nextPoint.setPoint(i, j + 1);
+        else
+            nextPoint.setPoint(i, j - 1);
+        return nextPoint;
+    }
+
+    private boolean checkLegit(int i, int j) {
+        return ((i >= 0) && (i < this.boardDim) && (j >= 0) && (j < this.boardDim) 
+                && this.internalBlocks[i][j] != 0);
+    }
+
     private Board moveLeft() {
         Board movedBoard = new Board(this);
         int iZero = findiZero();
         int jZero = findjZero();
         if (jZero > 0) {
-            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero][jZero-1];
+            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero][jZero - 1];
             movedBoard.internalBlocks[iZero][jZero-1] = 0;
             movedBoard.hammingScore = movedBoard.hamming();
             movedBoard.manhattanScore = movedBoard.manhattan();
@@ -228,8 +277,8 @@ public class Board {
         int iZero = findiZero();
         int jZero = findjZero();
         if (jZero < this.boardDim-1) {
-            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero][jZero+1];
-            movedBoard.internalBlocks[iZero][jZero+1] = 0;
+            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero][jZero + 1];
+            movedBoard.internalBlocks[iZero][jZero + 1] = 0;
         }
         return movedBoard;
     }
@@ -239,8 +288,8 @@ public class Board {
         int iZero = findiZero();
         int jZero = findjZero();
         if (iZero > 0) {
-            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero-1][jZero];
-            movedBoard.internalBlocks[iZero-1][jZero] = 0;
+            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero - 1][jZero];
+            movedBoard.internalBlocks[iZero - 1][jZero] = 0;
             movedBoard.hammingScore = movedBoard.hamming();
             movedBoard.manhattanScore = movedBoard.manhattan();
         }
@@ -252,8 +301,8 @@ public class Board {
         int iZero = findiZero();
         int jZero = findjZero();
         if (iZero < this.boardDim-1) {
-            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero+1][jZero];
-            movedBoard.internalBlocks[iZero+1][jZero] = 0;
+            movedBoard.internalBlocks[iZero][jZero] = this.internalBlocks[iZero + 1][jZero];
+            movedBoard.internalBlocks[iZero + 1][jZero] = 0;
         }
         return movedBoard;
     }
@@ -261,5 +310,58 @@ public class Board {
     private int abs(int x) {
         int y = (x >= 0) ? x : -x;
         return y;
+    }
+}
+
+class point {
+    private int i;
+    private int j;
+    private final int dim;
+
+    public point (int i, int j, int dim) {
+        this.dim = dim;
+        if ((i >= 0 ) && (i < dim) && (j >= 0) && (j < dim)) {
+            this.i = i;
+            this.j = j;
+        }
+        else {
+            throw new IllegalArgumentException("Threw an IllegalArgumentException");
+        }
+    }
+
+    public point (point p) {
+        this.dim = p.dim;
+        this.i = p.i;
+        this.j = p.j;
+    }
+
+    public int geti() {
+        return this.i;
+    }
+
+    public int getj() {
+        return this.j;
+    }
+
+    public int setPoint(point x) {
+        if ((this.i >= 0) && (this.i < dim) && (this.j >= 0) && (this.j < dim)) {
+            this.i = x.i;
+            this.j = x.j;
+            return 0;
+        }
+        else {
+            throw new IllegalArgumentException("Threw an IllegalArgumentException");
+        }
+    }
+
+    public int setPoint(int x, int y) {
+        if ((x >= 0 ) && (x < this.dim) && (y >= 0) && (y < this.dim)) {
+            this.i = x;
+            this.j = y;
+            return 0;
+        }
+        else {
+            throw new IllegalArgumentException("Threw an IllegalArgumentException");
+        }
     }
 }
