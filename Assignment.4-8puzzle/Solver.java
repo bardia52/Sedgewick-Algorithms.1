86a878e0af05339d50e5efc01b2fbc855f0170bb
Bardia Alavi <bardia_alavi@yahoo.com>
2018-09-17 01:03:06 -0400

    Add twin to check for solvable problems 98/100

--- Assignment.4-8puzzle/Solver.java
+++ Assignment.4-8puzzle/Solver.java
@@ -12,37 +12,49 @@
 import edu.princeton.cs.algs4.MinPQ;
 // import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.Stack;
-import edu.princeton.cs.algs4.StdOut;
+//import edu.princeton.cs.algs4.StdOut;
 
 public class Solver {
-    private final int distance;
-    private final Board initBoard;
-    private final Stack<Board> boards;
+    private final boolean solvable;    // Is this board solvable or not
+    private final Board initBoard;     // Initial board
+    private final Stack<Board> boards; // Sequence of boards
+    private final Board twinBoard;     // Twin of the initial board
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         boards = new Stack<Board>();
-        distance = initial.manhattan();
+        initial.manhattan();
         initBoard = initial;
+        twinBoard = initBoard.twin();
 
+        // Do everything the same for twin board
         MinPQ<SearchNode> pq = new MinPQ<SearchNode>();
+        MinPQ<SearchNode> pqTwin = new MinPQ<SearchNode>();
         SearchNode firstNode = new SearchNode(initBoard, 0, null);
+        SearchNode firstNodeTwin = new SearchNode(twinBoard, 0, null);
         pq.insert(firstNode);
+        pqTwin.insert(firstNodeTwin);
         while (true) {
             SearchNode minNode = pq.delMin();
-            // Check to see if in the same path
+            SearchNode minNodeTwin = pqTwin.delMin();
+
+            Board minBoardTwin = minNodeTwin.board;
+            if (minBoardTwin.isGoal()) {
+                this.solvable = false;
+                return;
+            }
             Board minBoard = minNode.board;
-            // reversedBoards.push(minBoard);
             if (minBoard.isGoal()) {
                 this.boards.push(minBoard);
                 while (minNode.previous != null) {
                     minNode = minNode.previous;
                     this.boards.push(minNode.board);
                 }
+                this.solvable = true;
                 return;
             }
             minNode.moves++;
-            // StdOut.print(minNode.moves + ": " + minBoard.manhattan() + " - " + minBoard.toString());
+            minNodeTwin.moves++;
             Iterable<Board> neighbors = minBoard.neighbors();
             for (Board neighbor : neighbors) {
                 if ((minNode.previous == null) || (!neighbor.equals(minNode.previous.board))) {
@@ -50,12 +62,19 @@ public class Solver {
                     pq.insert(sn);
                 }
             }
+            Iterable<Board> neighborsTwin = minBoardTwin.neighbors();
+            for (Board neighborTwin : neighborsTwin) {
+                if ((minNodeTwin.previous == null) || (!neighborTwin.equals(minNodeTwin.previous.board))) {
+                    SearchNode sn = new SearchNode(neighborTwin, minNodeTwin.moves, minNodeTwin);
+                    pqTwin.insert(sn);
+                }
+            }
         }
     }
 
     // is the initial board solvable?
     public boolean isSolvable() {
-        return (distance == 0);
+        return solvable;
     }
 
     // min number of moves to solve initial board; -1 if unsolvable
