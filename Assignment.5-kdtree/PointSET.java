edeea87ba7c3298d66a89fa953fa64250d3dc0e0
Bardia Alavi <bardia_alavi@yahoo.com>
2018-10-01 00:33:51 -0400

    Initial submission
    Initial submission for Kd-Trees assignment

---
+++ Assignment.5-kdtree/PointSET.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Name:    Bardia Alavi
+ *  NetID:   bardia
+ *  Precept: P01
+ *
+ *  Partner Name:    N/A
+ *  Partner NetID:   N/A
+ *  Partner Precept: N/A
+ * 
+ *  Description: 
+ ******************************************************************************/
+
+public class PointSET {
+
+}
