b7b57189656cfd5b0fbc5f42246a046515dc081d
Bardia Alavi <bardia_alavi@yahoo.com>
2018-11-11 00:03:11 -0500

    Fix size and isEmpty and contains for kdtree 53/100

--- Assignment.5-kdtree/KdTree.java
+++ Assignment.5-kdtree/KdTree.java
@@ -16,24 +16,24 @@ import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.Queue;
 
 public class KdTree {
-    private SET<KdTreeNode> pointTree;
     private int numPoints;
+    private KdTreeNode root;
 
     private class KdTreeNode implements Comparable<KdTreeNode> {
-        private boolean xAxis;
+        private boolean isXAxis;
         private Point2D point;
         KdTreeNode left;
         KdTreeNode right;
 
         public KdTreeNode(Point2D p) {
-            this.xAxis = true;
+            this.isXAxis = true;
             this.point = p;
             this.left = null;
             this.right = null;
         }
 
         public int compareTo(KdTreeNode that) {
-            if (xAxis) {
+            if (isXAxis) {
                 if (this.point.x() < that.point.x()) return -1;
                 else if (this.point.x() > that.point.x()) return +1;
                 else return 0;
@@ -49,7 +49,7 @@ public class KdTree {
     // construct an empty set of points
     public KdTree() {
         this.numPoints = 0;
-        this.pointTree = new SET<KdTreeNode>();
+        this.root = null;
     }
 
     // is the set empty?
@@ -63,35 +63,67 @@ public class KdTree {
     }
 
     // add the point to the set (if it is not already in the set)
+    /// @Todo: Fill this
     public void insert(Point2D p) {
         if (!contains(p)) {
-            KdTreeNode newNode = new KdTreeNode(p);
-            pointTree.add(newNode);
+            //KdTreeNode newNode = new KdTreeNode(p);
+            //pointTree.add(newNode);
             numPoints++;
         }
     }
 
     // does the set contain point p?
+    /// @Todo: Fill this
     public boolean contains(Point2D p) {
-        return true;
+        return contains(root, p, true);
+    }
+
+    private boolean contains(KdTreeNode node, Point2D p, boolean isXAxis) {
+        if (node == null)
+            return false;
+        else if (node.point == p) {
+            return true;
+        }
+        else {
+            if (isXAxis) {
+                if (p.x() < node.point.x()) {
+                    return contains(node.left, p, !isXAxis);
+                }
+                else {
+                    return contains(node.right, p, !isXAxis);
+                }
+            }
+            else {
+                if (p.x() < node.point.y()) {
+                    return contains(node.left, p, !isXAxis);
+                }
+                else {
+                    return contains(node.right, p, !isXAxis);
+                }
+            }
+        }
     }
 
     // draw all points to standard draw
     public void draw() {
+        /// @to-do: Fill this 
     }
 
     // all points that are inside the rectangle (or on the boundary)
+    /// @Todo: Fill this
     public Iterable<Point2D> range(RectHV rect) {
         Queue<Point2D> p2 = new Queue<Point2D>();
         return p2;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
+    /// @Todo: Fill this
     public Point2D nearest(Point2D p) {
         return p;
     }
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
+        /// @to-do: Fill this 
     }
 }
